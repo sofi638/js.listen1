6930d37ecb90e72fd747ee3d7d7176b168df2589
sophie <sofibibilashvili524@gmail.com>
2025-01-04 14:58:08 +0400

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,103 @@
+let name1 = "mariami";
+
+let age1 = 20
+
+// ეს არის კომენტარი ჯავასკრიპტში.
+
+
+let name2 = "sofi";
+
+let age2 = 17
+
+let color ="pink";
+
+let x1 = 10;
+let y2 = 20;
+
+let z1 = 20 + 30
+let z2 = 20 * 30
+let z3 = 20 - 30
+let z4 = 10 / 5
+let z5 = x1 + y2;
+
+console.log("hello");
+console.log("sofi bibilashvili")
+console.log("17")
+console.log("purple")
+
+console.log(z5)
+
+let num1 = 20;
+let num2 = 40;
+let num3 = num1 + num2;
+let num4 = num1 * num2;
+let num5 = num1 / num2;
+let num6 = num1 - num2;
+
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log(num5)
+console.log(num6)
+
+
+let num7 = 100;
+
+num7 = 200;
+
+console.log(100)
+console.log(num7)
+
+
+let age3 = 10;
+
+age2 = 11;
+
+
+ let p1 = document.querySelector(".p1")
+
+ console.log(p1)
+
+ p1.textContent = "sofi bibiashvili";
+ 
+ p1.style.color = "red";
+ p1.style.background = "black";
+ p1.style.padding = "20px";
+ p1.style.borderRadius = "20px";
+ p1.style.width = "200px";
+ p1.style.height = "300px";
+
+let p2 = document.querySelector(".p2")
+
+ p2.textContent = "text lorem";
+ p2.style.color = "pink";
+ p2.style.background = "blue";
+ p2.style.padding = "20px";
+ p2.style.borderRadius = "30px";
+ p2.style.width = "300px";
+ p2.style.height = "400px";
+ p2.style.border = " 10px solid green";
+ 
+ p2.style.display = "flex";
+ p2.style.textAlign = "center";
+ 
+let btn1 = document.querySelector(".btn1");
+console.log(btn1);
+
+btn1.addEventListener("click", () => {
+    console.log("button was clicked");
+
+    btn1.style.background = "red";
+    btn1.style.padding = "40px";
+})
+
+let btn2 = document.querySelector(".btn2")
+
+btn2.addEventListener("click", () => {
+ console.log(" button was clicked")
+
+    btn2.style.border = " 8px solid red"
+    btn2.style.width = "200px"
+} )
